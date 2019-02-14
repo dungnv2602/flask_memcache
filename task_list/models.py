5dbd9a92e1d3a3bc3363528d6d47b7dce819730e
dungnv2602 <dungnv2602@gmail.com>
2019-02-14 14:18:42 +0700

    Task table setup

---
+++ task_list/models.py
@@ -0,0 +1,10 @@
+from task_list import db
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text(), nullable=False)
+
+    def __repr__(self):
+        return f'<Task: {self.name}>'
+
